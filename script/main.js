797fe73fdcde81aeb8bcd8125187c7e2ede601e0
AseelFatayerji <161306291+AseelFatayerji@users.noreply.github.com>
2025-12-05 19:31:44 +0200

    added scroll animation

--- script/main.js
+++ script/main.js
@@ -9,3 +9,27 @@ navLinks.addEventListener("click", (e) => {
   navLinks.classList.remove("open");
   hamburger.checked = false;
 });
+
+const scrollRevealOPtion = {
+  distance: "50px",
+  duration: 1000,
+  origin: "bottom",
+};
+
+ScrollReveal().reveal(".header-img img", {
+  ...scrollRevealOPtion,
+  origin: "right",
+  interval: 500,
+});
+ScrollReveal().reveal(".header-content h1", {
+  ...scrollRevealOPtion,
+  delay: 500,
+});
+ScrollReveal().reveal(".header-content .section-description", {
+  ...scrollRevealOPtion,
+  delay: 2000,
+});
+ScrollReveal().reveal(".header-content form", {
+  ...scrollRevealOPtion,
+  delay: 2500,
+});
